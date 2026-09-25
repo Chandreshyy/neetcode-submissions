877c3c05054419e76498b7f75b6e4db91b3a74cc
Chandresh <YCHANDRESH141@GMAIL.COM>
2026-09-25 16:36:43 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        rows = len(board)
+        cols = len(board[0])
+        n = len(word)
+
+        def backtrack(i, j, index):
+            if index == n:
+                return True
+                
+            if i < 0 or j < 0 or i >= rows or j >= cols:
+                return False
+            
+            ori_char = board[i][j]
+            if ori_char != word[index]:
+                return False
+            
+            board[i][j] = '#'
+            result = (backtrack(i+1, j, index+1) 
+                    or backtrack(i, j+1, index+1)
+                    or backtrack(i-1, j, index+1)
+                    or backtrack(i, j-1, index+1)
+                )
+            board[i][j] = ori_char
+            return result
+        
+        for i in range(rows):
+            for j in range(cols):
+                if backtrack(i, j, 0):
+                    return True
+        return False
+        # print(result)
+        
+
